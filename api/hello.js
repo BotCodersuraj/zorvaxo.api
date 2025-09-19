7821d27e67b500e3199c60ee189ee108ecf124b3
BotCodersuraj <moharsingh7221@gmail.com>
2025-09-19 05:42:01 +0530

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Hello Suraj bhai 🚀, API is live!" });
+  res.status(200).json({ message: "Hello Suraj boss 🚀, API is live!" });
 }
